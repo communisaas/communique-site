186275fca39cb361c17350c7ec218023d5a89e2b
ejmockler <emockler@ucsc.edu>
2026-01-23 19:40:19 -0800

    chore: remove dead services
    Deleted orphaned service modules with 0 imports:
    - addressGeneration.ts
    - geolocation.ts
    - jurisdictionProvider.ts
    - personalization.ts
    - template-correction.ts
    - delivery/ (SMTP module)
    - aws/ (DynamoDB/SQS)
    
    Updated index.ts barrel exports.

--- src/lib/services/index.ts
+++ src/lib/services/index.ts
@@ -1,23 +1,10 @@
 // Services Module - Barrel Export
 export * from './apiClient';
-export * from './aws/dynamodb-rate-limiter';
-export * from './aws/sqs-client';
 export * from './channelResolver';
 export * from './emailService';
-export * from './geolocation';
-export * from './jurisdictionProvider';
-export * from './personalization';
 export * from './zipDistrictLookup';
 
 // Convenience exports
 export { api, analyticsApi } from './apiClient';
-export {
-	DynamoDBRateLimiter,
-	createRateLimiter,
-	extractChamber,
-	createRateLimitError
-} from './aws/dynamodb-rate-limiter';
-export { cwcSQSClient } from './aws/sqs-client';
 export { analyzeEmailFlow, launchEmail } from './emailService';
 export { resolveChannel } from './channelResolver';
-export { geolocateFromAddress } from './geolocation';
