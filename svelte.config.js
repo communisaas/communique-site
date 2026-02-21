849c8c201900601a33b389772ff726a403eb363b
ejmockler <emockler@ucsc.edu>
2026-02-21 09:04:53 -0700

    fix(csp): allow Nominatim geocoding API in connect-src
    Add https://nominatim.openstreetmap.org to CSP connect-src directive
    to unblock client-side location autocomplete.

--- svelte.config.js
+++ svelte.config.js
@@ -35,7 +35,7 @@ const config = {
 				'style-src': ['self', 'unsafe-inline', 'https://fonts.googleapis.com'],
 				'img-src': ['self', 'data:', 'blob:'],
 				'font-src': ['self', 'https://fonts.gstatic.com'],
-				'connect-src': ['self'],
+				'connect-src': ['self', 'https://nominatim.openstreetmap.org'],
 				'worker-src': ['self', 'blob:'],
 				'object-src': ['none'],
 				'frame-ancestors': ['none'],
